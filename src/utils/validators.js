64648dc72786a22ff1b91e68daf16f8c62850d6f
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-22 21:00:49 +0700

    check validators

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,19 +1,19 @@
-// const validateEmail = (email) => {
-//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   return emailRegex.test(email);
-// };
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
-// const validatePhoneNumber = (phoneNumber) => {
-//   const phoneRegex = /^[0-9]{9,11}$/;
-//   return phoneRegex.test(phoneNumber);
-// };
-// const isValidDate = (dateString) => {
-//   const regex = /^\d{4}-\d{2}-\d{2}$/;
-//   return dateString.match(regex) !== null;
-// };
+const validatePhoneNumber = (phoneNumber) => {
+  const phoneRegex = /^[0-9]{9,11}$/;
+  return phoneRegex.test(phoneNumber);
+};
+const isValidDate = (dateString) => {
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+  return dateString.match(regex) !== null;
+};
 
-// module.exports = {
-//   validateEmail,
-//   validatePhoneNumber,
-//   isValidDate
-// };
+module.exports = {
+  validateEmail,
+  validatePhoneNumber,
+  isValidDate
+};
