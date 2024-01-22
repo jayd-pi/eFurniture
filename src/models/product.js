188d5c96b42644309cf933feb7d2bb3e519d1fe7
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-22 21:09:10 +0700

    productModel

--- src/models/product.js
+++ src/models/product.js
@@ -1,3 +1,37 @@
 const mongoose = require("mongoose");
+const { isValidImageSize } = require("../utils/validators");
+
+const productSchema = new mongoose.Schema({
+    productName: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    description: {
+      type: String,
+      maxlength: 3000,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stockQuantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    images: {
+      type: String,
+      validate: {
+        validator: isValidImageSize,
+        message: "Image size must be less than 10MB.",
+      },
+    },
+  });
 
 module.exports = mongoose.model("Product", productSchema);
