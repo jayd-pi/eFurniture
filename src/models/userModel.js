997b58688a37afb7ba08b95ce1af2bccb1ac85c8
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-22 21:00:23 +0700

    userModel

---
+++ src/models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const { validateEmail, validatePhoneNumber } = require("../utils/validators");
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validateEmail,
+      message: (props) => `${props.value} is not a valid email address!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validatePhoneNumber,
+      message: (props) =>
+        `${props.value} is not a valid phone number! Must be 9-11 digits positive number.`,
+    },
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false,
+  },
+  cart: {
+    type: Array,
+    default: [],
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
