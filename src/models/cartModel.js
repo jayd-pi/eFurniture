274c34e80585a2c8b9146c0a114607b4e9a6fa30
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-22 21:15:13 +0700

    cartModel

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose"); // Erase if already required
+const mongoose = require("mongoose"); 
 
-// Declare the Schema of the Mongo model
+
 const cartSchema = new mongoose.Schema(
   {
     products: [
@@ -24,5 +24,4 @@ const cartSchema = new mongoose.Schema(
   }
 );
 
-//Export the model
 module.exports = mongoose.model("Cart", cartSchema);
