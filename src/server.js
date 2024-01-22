ca64ed880e70da763c1f41d804133c602ea2bdf5
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-22 21:38:21 +0700

    update link test api

--- src/server.js
+++ src/server.js
@@ -5,12 +5,15 @@ const morgan = require('morgan');
 const port = process.env.PORT || 9000
 const dotenv = require('dotenv').config();
 const dbConnect = require('./config/dbConnect');
+const userRouter = require('./routes/userRoutes');
 
 
 dbConnect();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
+
+app.use("/api/v1/user", userRouter);
 
 
 app.listen(port, ()=>{
