dd7c6b0a0f42a45d1d4c756b656ac5a389861cb0
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-23 08:40:49 +0700

    bcrypt password

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const { validateEmail, validatePhoneNumber, isValidDate } = require("../utils/validators");
+const {
+  validateEmail,
+  validatePhoneNumber,
+  isValidDate,
+} = require("../utils/validators");
 
 const userSchema = new mongoose.Schema({
   firstname: {
@@ -40,7 +44,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: isValidDate,
-      message: props => `${props.value} is not a valid date. Use the format YYYY-MM-DD.`,
+      message: (props) =>
+        `${props.value} is not a valid date. Use the format YYYY-MM-DD.`,
     },
   },
   isAdmin: {
@@ -61,5 +66,27 @@ const userSchema = new mongoose.Schema({
     enum: ["male", "female", "other"],
   },
 });
+//bcrypt password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000;
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", userSchema);
