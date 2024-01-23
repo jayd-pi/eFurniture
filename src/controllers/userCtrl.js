bac82f0e8f8ec13d5935178c17446167c1d62467
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-23 08:56:02 +0700

    featured create User

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -5,6 +5,23 @@ const { generateRefreshToken } = require("../config/generateRefreshToken");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 
+//Register account
+
+const createUser = asyncHandler(async (req, res) => {
+    try {
+      const { email } = req.body;
+      const existingEmail = await User.findOne({ email });
+      if (existingEmail) {
+        res.status(400).json({ error: "User with this email already exists" });
+      }
+      const newUser = new User(req.body);
+      const user = await newUser.save();
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
 module.exports = {
-    
+    createUser
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const {authMiddleware, isAdmin} = require("../middlewares/authMiddleware")
 const {
+    createUser
+} = require("../controllers/userCtrl");
 
-} = require("../controllers/userCtrl")
+router.post("/register",createUser);
 
 module.exports = router
