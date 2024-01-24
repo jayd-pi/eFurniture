86053fa0e1262fb229111d4ed7bbbcb16ff1e956
Phuoc Nguyen <huuphuocnguyen2002@gmail.com>
2024-01-24 08:33:54 +0700

    update lib

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,5 +6,7 @@ const {
 } = require("../controllers/userCtrl");
 
 router.post("/register",createUser);
+// router.post("/login", loginUserCtrl);
+// router.post("/admin-login", loginAdmin);
 
 module.exports = router
