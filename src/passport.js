d9b1186b82d8a45bcb0b90e656f13037f584bde4
Huỳnh Nguyễn Tuấn Duy <andyhntd2003@gmail.com>
2024-01-25 11:50:03 +0700

    facebook login strategy

--- src/passport.js
+++ src/passport.js
@@ -29,7 +29,18 @@ passport.use(
       }
     )
   );
-  
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_APP_ID,
+        clientSecret: process.env.FACEBOOK_APP_SECRET,
+        callbackURL: "/auth/facebook/callback",
+      },
+      function (accessToken, refreshToken, profile, done) {
+        done(null, profile);
+      }
+    )
+  );
   
   passport.serializeUser((user, done) => {
     done(null, user);
