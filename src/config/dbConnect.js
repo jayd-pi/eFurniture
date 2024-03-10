85d17210333b40f281d391c99eb02acb41f90984
viet <viethxse@gmail.com>
2024-03-10 18:48:56 +0700

    x

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    mongoose.set('strictQuery', false);
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("connect successfully");
   } catch (error) {
